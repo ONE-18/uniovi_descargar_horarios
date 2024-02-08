26cd7292ffb359023f36a377676ee2aba4f749d7
juan granda <juangranda2002@gmail.com>
2024-02-08 11:44:59 +0100

    problemas por anuncio de baja batería

--- main.py
+++ main.py
@@ -28,43 +28,24 @@ def append_table_to_txt(driver):
     month_date = driver.find_element(By.XPATH, '//*[@id="j_id_71:j_id_7e_container"]/div[1]/div[3]/h2').text
     
     try:
-        clases = []
-        days_n = []
-        
-        days = driver.find_elements(By.CSS_SELECTOR, "[class*='fc-day']")
-        for day in days:
-            days_n.append(day.get_attribute('data-date')) if day.get_attribute('data-date') not  in days_n else None
-        
+
         clases = driver.find_elements(By.CSS_SELECTOR, "[class*='fc-event-container']")
         for clase in clases:
             clase.click()
-            popup = driver.find_element(By.CSS_SELECTOR,"[class*='ui-dialog']")
-            print(popup.text)
-        
-        
-        # # Lunes
-        # clases.append(f"Lunes {fecha_completa.strftime('%d/%m/%Y')}")
-        # clases.append(table[1].text + '\n')
-        
-        # # Martes
-        # fecha_completa = fecha_completa + timedelta(days=1)
-        # clases.append(f"Martes {fecha_completa.strftime('%d/%m/%Y')}")
-        # clases.append(table[3].text + '\n')
-
-        # # Miercoles
-        # fecha_completa = fecha_completa + timedelta(days=1)
-        # clases.append(f"Miercoles {fecha_completa.strftime('%d/%m/%Y')}")
-        # clases.append(table[5].text + '\n')
-        
-        # # Jueves
-        # fecha_completa = fecha_completa + timedelta(days=1)
-        # clases.append(f"Jueves {fecha_completa.strftime('%d/%m/%Y')}")
-        # clases.append(table[7].text + '\n')
-        
-        # # Viernes
-        # fecha_completa = fecha_completa + timedelta(days=1)
-        # clases.append(f"Viernes {fecha_completa.strftime('%d/%m/%Y')}")
-        # clases.append(table[9].text + '\n')
+            text = driver.find_element(By.CSS_SELECTOR,"[class*='ui-dialog']").text
+            
+            fecha_str = text[2].split(' ')[1]
+            fecha = datetime.strptime(fecha_str, "%d/%m/%Y")
+            hora_ini = text[2].split(' ')[2]
+            hora_fin = text[3].split(' ')[1]
+            aula = text[1].split(' - ')[1]
+            asignatura = text[0].split(' - ')[0]
+            clase_tipo = text[0].split(' - ')[1]
+            
+            clase_str = f'{fecha} {fecha_str}\n({hora_ini} - {hora_fin})\t{asignatura}\n{aula}\t{clase_tipo}'
+            
+            with open(output_file, 'a', encoding='utf-8') as file:
+                file.write(clase_str)
 
         # # Añadir los datos al archivo de texto
         # with open(output_file, 'a', encoding='utf-8') as file:
